9ee84783f6f161b82a28edbeb28ab3b49c753f34
tbokhar <tbokhar@ncsu.edu>
2019-01-09 18:57:00 -0500

    Created project structure

---
+++ KenobiCode/src/main/java/frc/robot/drive/DriveCom.java
@@ -0,0 +1,12 @@
+package frc.robot.drive;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveCom extends Command {
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+}
---
+++ KenobiCode/src/main/java/frc/robot/drive/DriveTrainSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.drive;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrainSubsystem extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+}
