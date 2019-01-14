398d2a6db2a0dd1fdb1b750724ccd42710de6e47
Nickolos Monk <44507976+Fryro@users.noreply.github.com>
2019-01-14 13:33:43 -0500

    Added port assignment for SpecFunc

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,6 +13,8 @@ public class RobotMap {
   public static int leftSlaveId = 2;
   public static int rightMasterId = 3; //CAN values
   public static int rightSlaveId = 0;
+  public static int specfuncWench = 5; //CAN values 
+      //CAN values are assigned to Victor motor controllers
   
   public static int LEFT_ENC_A = 2;
   public static int LEFT_ENC_B = 3;
