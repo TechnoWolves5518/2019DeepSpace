ed040d465947e9c16272f90eb354580b184d6fc0
Nickolos Monk <44507976+Fryro@users.noreply.github.com>
2019-03-19 17:58:37 -0400

    Changes made to 'ClimbGroup'
    -Added the rest of the sequential commands.

--- src/main/java/frc/robot/auto/ClimbGroup.java
+++ src/main/java/frc/robot/auto/ClimbGroup.java
@@ -5,7 +5,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ClimbGroup extends CommandGroup {
 
     public ClimbGroup() {
-        addSequential(new ActivateClimbPistons(true, false));
+        addSequential(new ActivateClimbPistons(true, false)); //Extends front pistons, retracts back piston
+        addSequential(new AutoDriveForward(2)); //THE TWO IS AN UNTESTED NUMBER THAT NEEDS TO BE TESTED
+        addSequential(new ActivateClimbPistons(false, true)); //Retracts front piston, extends back piston
+        addSequential(new AutoDriveForward(2)); //THIS TWO IS ALSO UNTESTED
+        addSequential(new ActivateClimbPistons(false, false)); //Retracts both pistons
+        addSequential(new AutoDriveForward(2)); //yeah this one too
     }
 
 }
